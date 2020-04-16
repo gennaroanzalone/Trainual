a3ed768ee0c374596a163e659e05523b1f8f44db
Gennaro Anzalone <g.anzalone12@gmail.com>
2020-04-15 18:03:51 -0700

    change redirect to user list after creating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,11 +45,12 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.html {redirect_to users_path, notice: 'User was successfully updated.' }
+        format.js
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.js
       end
     end
   end

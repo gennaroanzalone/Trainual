83385173668f539ed550cbce63fdd3b595dbb03e
Gennaro Anzalone <g.anzalone12@gmail.com>
2020-04-15 18:05:37 -0700

    add validation for unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  before_validation :downcase_email
+  validates :email, uniqueness: true
+
+
+  private
+
+  def downcase_email
+    self.email = email.downcase if email.present?
+  end
 end

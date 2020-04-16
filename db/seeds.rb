cb4a2170f5ab1cce37950de5519dfe8f20d6ba96
Gennaro Anzalone <g.anzalone12@gmail.com>
2020-04-16 06:28:16 -0700

    increase seeds to 1000

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-100.times do
+1000.times do
   User.create(
     name: Faker::Name.name,
     email: Faker::Internet.safe_email,

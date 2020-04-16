010b1d4d411392020d8824cdcba3ee51b2be393e
Gennaro Anzalone <g.anzalone12@gmail.com>
2020-04-16 06:26:38 -0700

    rollback of code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
   # GET /users/new
   def new
     @user = User.new
+    @user = User.find(params[:id])
     respond_to do |format|
       format.html
       format.js
@@ -43,12 +44,11 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @users = User.all
     @user = User.new(user_params)
+
     respond_to do |format|
       if @user.save
         format.html {redirect_to users_path, notice: 'User was successfully updated.' }
-        format.js
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -60,13 +60,10 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    @user = User.find(params[:id])
-    @users = User.all
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to users_path, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
-        format.js
       else
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
